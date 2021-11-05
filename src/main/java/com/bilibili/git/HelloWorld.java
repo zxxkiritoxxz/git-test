a0f286e261af070add2b76ed7e2a394a8e5d9c71
tom_glb <goodMoring_glb@guigu.com>
2021-11-05 10:39:09 +0800

    push test

--- src/main/java/com/bilibili/git/HelloWorld.java
+++ src/main/java/com/bilibili/git/HelloWorld.java
@@ -14,5 +14,6 @@ public class HelloWorld {
         System.out.println("HelloWorld2");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
