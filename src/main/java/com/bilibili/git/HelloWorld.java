87c9ba8da0c71894a7722a338883b8f2da673498
tom_glb <goodMoring_glb@guigu.com>
2021-11-05 09:01:44 +0800

    first commit

---
+++ src/main/java/com/bilibili/git/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.bilibili.git;
+
+/**
+ * ClassName: HelloWorld <br/>
+ * Description: <br/>
+ * date: 2021/11/5 8:50<br/>
+ *
+ * @author kirito <br/>
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
