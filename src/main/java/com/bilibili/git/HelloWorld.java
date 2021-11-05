eac1c311dc1dd274c54c95fae311145c454b1c2d
tom_glb <goodMoring_glb@guigu.com>
2021-11-05 09:03:31 +0800

    second commit

--- src/main/java/com/bilibili/git/HelloWorld.java
+++ src/main/java/com/bilibili/git/HelloWorld.java
@@ -10,5 +10,6 @@ package com.bilibili.git;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("HelloWorld1");
     }
 }
